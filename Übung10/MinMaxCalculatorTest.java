e2c76ececda53423c1cf27502a1ee2dc0b963d8a
jonasweigt42 <jonas.weigt42@gmail.com>
2016-06-09 16:10:15 +0200

    added one more testcase

--- Übung10/MinMaxCalculatorTest.java
+++ Übung10/MinMaxCalculatorTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
  * @author Jonas Weigt
  *	09.06.2016
  *  15:26:12
- *  
  *  Ist sicher noch erweiterbar!
  */
 
@@ -23,33 +22,48 @@ public class MinMaxCalculatorTest{
 	public void test() {
 		
 		
-		int[] arr200 = {-200, -25, 25, 200};
-		int[] arr000 = {0,10,25,2,3,5};
-		int[] arr001 = {-12,-2,-6, 0};
-		MinMaxCalculator calc200 = new MinMaxCalculator(arr200);
-		MinMaxCalculator calc000 = new MinMaxCalculator(arr000);
-		MinMaxCalculator calc001 = new MinMaxCalculator(arr001);
+		int[] testArray001 = {-200, -25, 25, 200};
+		int[] testArray002 = {0,10,25,2,3,5};
+		int[] testArray003 = {-12,-2,-6, 0};
+		int[] testArray004 = {-12, -3,-5,-7,-8};
+		MinMaxCalculator testCalc001 = new MinMaxCalculator(testArray001);
+		MinMaxCalculator testCalc002 = new MinMaxCalculator(testArray002);
+		MinMaxCalculator testCalc003 = new MinMaxCalculator(testArray003);
+		MinMaxCalculator testCalc004 = new MinMaxCalculator(testArray004);
 		
 		/*
 		 * Testcase1
+		 * negative Zahl ist Minimum und positive ist Maximum
 		 */
-		if(calc200.getMin() == -200 && calc200.getMax() == 200){
+		if(testCalc001.getMin() == -200 && testCalc001.getMax() == 200){
 			erfolgreich();
 		}else{
 			nichtEfolgreich();
 		}
 		/*
-		 * Testcase2
+		 * Testcase2 
+		 * negative Zahl ist Minimum und negative Zahl ist Maximum
 		 */
-		if(calc000.getMin() == 0 && calc000.getMax() == 25){
+		if(testCalc004.getMin() == -12 && testCalc004.getMax() == -3){
 			erfolgreich();
 		}else{
 			nichtEfolgreich();
 		}
+		
 		/*
 		 * Testcase3
+		 * 0 ist Minimum und positive Zahl ist Maimum
 		 */
-		if(calc001.getMin() == -12 && calc001.getMax() == 0){
+		if(testCalc002.getMin() == 0 && testCalc002.getMax() == 25){
+			erfolgreich();
+		}else{
+			nichtEfolgreich();
+		}
+		/*
+		 * Testcase4
+		 * negative Zahl ist Minimum und 0 ist Maximum 
+		 */
+		if(testCalc003.getMin() == -12 && testCalc003.getMax() == 0){
 			erfolgreich();
 		}else{
 			nichtEfolgreich();
